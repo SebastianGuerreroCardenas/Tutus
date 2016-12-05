10af35d640bfea8584d702f71bd93ee66dbcc4d1
Mark Vella <markvella@Marks-MacBook.local>
2016-12-05 18:06:31 -0500

    Added location creation client, storyboard, and view controller"

---
+++ Tutus/LocationCreationViewController.swift
@@ -0,0 +1,63 @@
+//
+//  LocationCreationViewController.swift
+//  Tutus
+//
+//  Created by Mark Vella on 12/5/16.
+//  Copyright © 2016 Sebastian Guerrero. All rights reserved.
+//
+
+import UIKit
+
+class LocationCreationViewController: BaseViewController {
+
+    var loginClient = LoginClient()
+    var locationClient = LocationClient()
+    var locationInfo = [String: String]()
+    var isEdit = false
+    
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var submitButton: UIButton!
+    @IBOutlet weak var pageTitle: UILabel!
+    
+    @IBAction func submitButtonTapped(sender: UIButton) {
+        self.locationInfo["name"] = self.nameField.text
+        self.locationInfo["isEdit"] = String(self.isEdit)
+        // if we are editing, the dictionary that was passed in also contains an eventId
+        
+        locationClient.setDict(diction: locationInfo) {
+            self.locationClient.createLocation(){ dict in
+                print(self.locationClient.dict)
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if !self.locationInfo.isEmpty {
+            //if we are editing, populate the fields and indicate this
+            self.populateFields()
+            self.isEdit = true
+            self.submitButton.setTitle("Edit Location",for: .normal)
+            self.pageTitle.text = "Edit Location"
+        }
+        
+        if !loginClient.isLoggedIn(){
+            print("is not logged in")
+            let loginStoryboard = UIStoryboard(name: "Login", bundle: nil)
+            let controller = loginStoryboard.instantiateViewController(withIdentifier: "LoginController") as UIViewController
+            present(controller, animated: true, completion: nil)
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func populateFields() {
+        self.nameField.text = self.locationInfo["name"]
+    }
+
+
+}
