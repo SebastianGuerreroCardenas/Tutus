36950d525977f9cf4abc48a72ea4810f1d02689a
Sebastian Guerrero <sguerrer@andrew.cmu.edu>
2016-12-13 12:29:21 -0500

    event

--- TutusTests/TutusTests.swift
+++ TutusTests/TutusTests.swift
@@ -30,6 +30,7 @@ class TutusTests: XCTestCase {
     override func setUp() {
         super.setUp()
         mainUser.dict["id"] = "tokenhere" as AnyObject?
+        currentEventObject.id = 
         // Put setup code here. This method is called before the invocation of each test method in the class.
         
     }
